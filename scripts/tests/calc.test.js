75f99da6ae14bb7b6ec41b26068ed5d38ad6df46
decant09 <>
2023-08-05 17:01:03 +0000

    Write test

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,22 @@
+const { TestScheduler } = require('jest');
+const addition = require("../calc");
+
+describe("calculator", () => {
+    describe("Addition function", () => {
+        test("Should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("Should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
+    });
+    describe("Subtraction function", () => {
+
+    });
+    describe("Multiplication function", () => {
+
+    });
+    describe("Division function", () => {
+
+    });
+});
